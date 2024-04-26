81381d882df2e3846c6ffe1b717a077d82c12b14
lavaman131 <lavaalex3@gmail.com>
2024-04-26 14:18:22 -0400

    added rendering code

--- src/mgpt/constants/__init__.py
+++ src/mgpt/constants/__init__.py
@@ -2,8 +2,9 @@ from pathlib import Path
 
 THIRD_PARTY_DIR = Path(__file__).resolve().parents[3].joinpath("third_party")
 
-# CHANGE HERE FOR YOUR DATA
+# CHANGE HERE TO PATHS FOR YOUR CONFIGURATION
 BABEL_DATA_DIR = Path("/projectnb/ivc-ml/alavaee/data/babel")
 HUMANML_DATA_DIR = Path("/projectnb/ivc-ml/alavaee/data/humanml")
 EXTRAPOLATION_DIR = Path("/projectnb/ivc-ml/alavaee/data/babel/extrapolation")
 PRECOMPUTED_DIR = Path("/projectnb/ivc-ml/alavaee/data/precomputed")
+#############################################
--- src/mgpt/render/matplot/plot_script.py
+++ src/mgpt/render/matplot/plot_script.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import mpl_toolkits.mplot3d.axes3d as p3
+
 # import cv2
 from textwrap import wrap
+import os
 
 
 def list_cut_average(ll, intervals):
@@ -23,22 +25,55 @@ def list_cut_average(ll, intervals):
     return ll_new
 
 
-def plot_3d_motion_mix(save_path, kinematic_tree, joints, title, dataset, figsize=(3, 3), fps=120, radius=3, vis_mode='default', lengths = []):
-    matplotlib.use('Agg')
+def plot_3d_motion_mix(
+    save_path,
+    kinematic_tree,
+    joints,
+    title,
+    dataset,
+    figsize=(3, 3),
+    fps=120,
+    radius=3,
+    vis_mode="default",
+    lengths=[],
+):
+    matplotlib.use("Agg")
     data = joints.copy().reshape(len(joints), -1, 3)
     frames_number = data.shape[0]
     frame_colors = []
-    if vis_mode == 'alternate':
+    if vis_mode == "alternate":
         for i, length in enumerate(lengths):
-            frame_colors += ['purple'] * length if i % 2 == 1 else ['orange'] * length
-    elif vis_mode == 'gt':
-        frame_colors = ['blue'] * frames_number
+            frame_colors += ["purple"] * length if i % 2 == 1 else ["orange"] * length
+    elif vis_mode == "gt":
+        frame_colors = ["blue"] * frames_number
     else:
         raise NotImplementedError
-    plot_3d_motion_multicolor(save_path, kinematic_tree, joints, title, dataset, figsize=figsize, fps=fps, radius=radius, vis_mode=vis_mode, frame_colors=frame_colors)
+    plot_3d_motion_multicolor(
+        save_path,
+        kinematic_tree,
+        joints,
+        title,
+        dataset,
+        figsize=figsize,
+        fps=fps,
+        radius=radius,
+        vis_mode=vis_mode,
+        frame_colors=frame_colors,
+    )
 
 
-def plot_3d_motion_multicolor(save_path, kinematic_tree, joints, title, dataset, figsize=(3, 3), fps=120, radius=3, vis_mode="default", frame_colors=[]):
+def plot_3d_motion_multicolor(
+    save_path,
+    kinematic_tree,
+    joints,
+    title,
+    dataset,
+    figsize=(3, 3),
+    fps=120,
+    radius=3,
+    vis_mode="default",
+    frame_colors=[],
+):
     """
     outputs the 3D motion to an mp4 file
     """
@@ -59,7 +94,12 @@ def plot_3d_motion_multicolor(save_path, kinematic_tree, joints, title, dataset,
 
     def plot_xzPlane(minx, maxx, miny, minz, maxz):
         ## Plot a plane XZ
-        verts = [[minx, miny, minz], [minx, miny, maxz], [maxx, miny, maxz], [maxx, miny, minz]]
+        verts = [
+            [minx, miny, minz],
+            [minx, miny, maxz],
+            [maxx, miny, maxz],
+            [maxx, miny, minz],
+        ]
         xz_plane = Poly3DCollection([verts])
         xz_plane.set_facecolor((0.5, 0.5, 0.5, 0.5))
         ax.add_collection3d(xz_plane)
@@ -72,9 +112,8 @@ def plot_3d_motion_multicolor(save_path, kinematic_tree, joints, title, dataset,
     # preparation related to specific datasets
     if dataset == "humanml":
         data *= 1.3  # scale for visualization
-    elif dataset =='babel':
+    elif dataset == "babel":
         data *= 1.3
-
 
     fig = plt.figure(figsize=figsize)
     plt.tight_layout()
@@ -83,18 +122,29 @@ def plot_3d_motion_multicolor(save_path, kinematic_tree, joints, title, dataset,
     MINS = data.min(axis=0).min(axis=0)
     MAXS = data.max(axis=0).max(axis=0)
     colors_blue = ["#4D84AA", "#5B9965", "#61CEB9", "#34C1E2", "#80B79A"]  # GT color
-    colors_orange = ["#DD5A37", "#D69E00", "#B75A39", "#FF6D00", "#DDB50E"]  # Generation color
+    colors_orange = [
+        "#DD5A37",
+        "#D69E00",
+        "#B75A39",
+        "#FF6D00",
+        "#DDB50E",
+    ]  # Generation color
     colors_purple = ["#6B31DB", "#AD40A8", "#AF2B79", "#9B00FF", "#D836C1"]
 
     colors_upper_body = colors_blue[:2] + colors_orange[2:]
 
-    colors_dict = {"blue": colors_blue, "orange": colors_orange, "purple": colors_purple, "upper_body": colors_upper_body}
+    colors_dict = {
+        "blue": colors_blue,
+        "orange": colors_orange,
+        "purple": colors_purple,
+        "upper_body": colors_upper_body,
+    }
 
     colors = colors_orange
-    if vis_mode == 'upper_body':  # lower body taken fixed to input motion
+    if vis_mode == "upper_body":  # lower body taken fixed to input motion
         colors[0] = colors_blue[0]
         colors[1] = colors_blue[1]
-    elif vis_mode == 'gt':
+    elif vis_mode == "gt":
         colors = colors_blue
 
     frame_number = data.shape[0]
@@ -113,29 +163,58 @@ def plot_3d_motion_multicolor(save_path, kinematic_tree, joints, title, dataset,
         ax.dist = 7.5
         if len(title) > 1:
             fig.suptitle(title[index], fontsize=10)
-        plot_xzPlane(MINS[0] - trajec[index, 0], MAXS[0] - trajec[index, 0], 0, MINS[2] - trajec[index, 2], MAXS[2] - trajec[index, 2])
+        plot_xzPlane(
+            MINS[0] - trajec[index, 0],
+            MAXS[0] - trajec[index, 0],
+            0,
+            MINS[2] - trajec[index, 2],
+            MAXS[2] - trajec[index, 2],
+        )
 
-        used_colors = colors_dict[frame_colors[index]] if (index < len(frame_colors)) else colors_dict["blue"]
+        used_colors = (
+            colors_dict[frame_colors[index]]
+            if (index < len(frame_colors))
+            else colors_dict["blue"]
+        )
         for i, (chain, color) in enumerate(zip(kinematic_tree, used_colors)):
             if i < 5:
                 linewidth = 4.0
             else:
                 linewidth = 2.0
-            ax.plot3D(data[index, chain, 0], data[index, chain, 1], data[index, chain, 2], linewidth=linewidth, color=color)
-        
+            ax.plot3D(
+                data[index, chain, 0],
+                data[index, chain, 1],
+                data[index, chain, 2],
+                linewidth=linewidth,
+                color=color,
+            )
+
         plt.axis("off")
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         ax.set_zticklabels([])
 
-    ani = FuncAnimation(fig, update, frames=frame_number, interval=1000 // fps, repeat=False)
+    ani = FuncAnimation(
+        fig, update, frames=frame_number, interval=1000 // fps, repeat=False
+    )
 
     ani.save(save_path, fps=fps)
 
     plt.close()
 
 
-def plot_3d_motion_multicolor_pdf(save_path, kinematic_tree, joints, title, dataset, figsize=(3, 3), fps=120, radius=3, vis_mode="default", frame_colors=[]):
+def plot_3d_motion_multicolor_pdf(
+    save_path,
+    kinematic_tree,
+    joints,
+    title,
+    dataset,
+    figsize=(3, 3),
+    fps=120,
+    radius=3,
+    vis_mode="default",
+    frame_colors=[],
+):
     """
     outputs the 3D motion to an mp4 file
     """
@@ -156,7 +235,12 @@ def plot_3d_motion_multicolor_pdf(save_path, kinematic_tree, joints, title, data
 
     def plot_xzPlane(minx, maxx, miny, minz, maxz):
         ## Plot a plane XZ
-        verts = [[minx, miny, minz], [minx, miny, maxz], [maxx, miny, maxz], [maxx, miny, minz]]
+        verts = [
+            [minx, miny, minz],
+            [minx, miny, maxz],
+            [maxx, miny, maxz],
+            [maxx, miny, minz],
+        ]
         xz_plane = Poly3DCollection([verts])
         xz_plane.set_facecolor((0.5, 0.5, 0.5, 0.5))
         ax.add_collection3d(xz_plane)
@@ -169,25 +253,35 @@ def plot_3d_motion_multicolor_pdf(save_path, kinematic_tree, joints, title, data
     # preparation related to specific datasets
     if dataset == "humanml":
         data *= 1.3  # scale for visualization
-    elif dataset =='babel':
+    elif dataset == "babel":
         data *= 1.3
-
 
     MINS = data.min(axis=0).min(axis=0)
     MAXS = data.max(axis=0).max(axis=0)
     colors_blue = ["#4D84AA", "#5B9965", "#61CEB9", "#34C1E2", "#80B79A"]  # GT color
-    colors_orange = ["#DD5A37", "#D69E00", "#B75A39", "#FF6D00", "#DDB50E"]  # Generation color
+    colors_orange = [
+        "#DD5A37",
+        "#D69E00",
+        "#B75A39",
+        "#FF6D00",
+        "#DDB50E",
+    ]  # Generation color
     colors_purple = ["#6B31DB", "#AD40A8", "#AF2B79", "#9B00FF", "#D836C1"]
 
     colors_upper_body = colors_blue[:2] + colors_orange[2:]
 
-    colors_dict = {"blue": colors_blue, "orange": colors_orange, "purple": colors_purple, "upper_body": colors_upper_body}
+    colors_dict = {
+        "blue": colors_blue,
+        "orange": colors_orange,
+        "purple": colors_purple,
+        "upper_body": colors_upper_body,
+    }
 
     colors = colors_orange
-    if vis_mode == 'upper_body':  # lower body taken fixed to input motion
+    if vis_mode == "upper_body":  # lower body taken fixed to input motion
         colors[0] = colors_blue[0]
         colors[1] = colors_blue[1]
-    elif vis_mode == 'gt':
+    elif vis_mode == "gt":
         colors = colors_blue
 
     frame_number = data.shape[0]
@@ -213,21 +307,32 @@ def plot_3d_motion_multicolor_pdf(save_path, kinematic_tree, joints, title, data
         ax.dist = 7.5
         if len(title) > 1:
             fig.suptitle(title[index], fontsize=10)
-        #plot_xzPlane(MINS[0] - trajec[index, 0], MAXS[0] - trajec[index, 0], 0, MINS[2] - trajec[index, 2], MAXS[2] - trajec[index, 2])
+        # plot_xzPlane(MINS[0] - trajec[index, 0], MAXS[0] - trajec[index, 0], 0, MINS[2] - trajec[index, 2], MAXS[2] - trajec[index, 2])
 
-        used_colors = colors_dict[frame_colors[index]] if (index < len(frame_colors)) else colors_dict["blue"]
+        used_colors = (
+            colors_dict[frame_colors[index]]
+            if (index < len(frame_colors))
+            else colors_dict["blue"]
+        )
         for i, (chain, color) in enumerate(zip(kinematic_tree, used_colors)):
             if i < 5:
                 linewidth = 4.0
             else:
                 linewidth = 2.0
-            ax.plot3D(data[index, chain, 0], data[index, chain, 1], data[index, chain, 2], linewidth=linewidth, color=color)
-        
+            ax.plot3D(
+                data[index, chain, 0],
+                data[index, chain, 1],
+                data[index, chain, 2],
+                linewidth=linewidth,
+                color=color,
+            )
+
         plt.axis("off")
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         ax.set_zticklabels([])
         import os
+
         os.makedirs(save_path.replace(".mp4", "/pdf"), exist_ok=True)
         plt.savefig(save_path.replace(".mp4", "/pdf") + str(index) + ".pdf")
         # clear the figure
@@ -235,17 +340,28 @@ def plot_3d_motion_multicolor_pdf(save_path, kinematic_tree, joints, title, data
         plt.close()
 
 
-def plot_3d_motion(save_path, kinematic_tree, joints, title, dataset, figsize=(3, 3), fps=120, radius=3, vis_mode='default', gt_frames=[]):
-    matplotlib.use('Agg')
+def plot_3d_motion(
+    save_path,
+    kinematic_tree,
+    joints,
+    title,
+    dataset,
+    figsize=(3, 3),
+    fps=120,
+    radius=3,
+    vis_mode="default",
+    gt_frames=[],
+):
+    matplotlib.use("Agg")
 
-    title_max_width = len(title) // 3 # max 3 lines
+    title_max_width = len(title) // 3  # max 3 lines
     title_size = 10 if title_max_width < 25 else 6
-    title = '\n'.join(wrap(title, title_max_width))
+    title = "\n".join(wrap(title, title_max_width))
 
     def init():
         ax.set_xlim3d([-radius / 2, radius / 2])
         ax.set_ylim3d([0, radius])
-        ax.set_zlim3d([-radius / 3., radius * 2 / 3.])
+        ax.set_zlim3d([-radius / 3.0, radius * 2 / 3.0])
         # print(title)
         fig.suptitle(title, fontsize=title_size)
         ax.grid(b=False)
@@ -256,7 +372,7 @@ def plot_3d_motion(save_path, kinematic_tree, joints, title, dataset, figsize=(3
             [minx, miny, minz],
             [minx, miny, maxz],
             [maxx, miny, maxz],
-            [maxx, miny, minz]
+            [maxx, miny, minz],
         ]
         xz_plane = Poly3DCollection([verts])
         xz_plane.set_facecolor((0.5, 0.5, 0.5, 0.5))
@@ -277,9 +393,11 @@ def plot_3d_motion(save_path, kinematic_tree, joints, title, dataset, figsize=(3
     data = joints.copy().reshape(len(joints), -1, 3)
 
     # preparation related to specific datasets
-    if dataset == 'kit':
+    if dataset == "kit":
         data *= 0.003  # scale for visualization
-    elif dataset in ['humanml', ]:
+    elif dataset in [
+        "humanml",
+    ]:
         data *= 1.3  # scale for visualization
 
     fig = plt.figure(figsize=figsize)
@@ -289,15 +407,25 @@ def plot_3d_motion(save_path, kinematic_tree, joints, title, dataset, figsize=(3
     MINS = data.min(axis=0).min(axis=0)
     MAXS = data.max(axis=0).max(axis=0)
     colors_blue = ["#4D84AA", "#5B9965", "#61CEB9", "#34C1E2", "#80B79A"]  # GT color
-    colors_orange = ["#DD5A37", "#D69E00", "#B75A39", "#FF6D00", "#DDB50E"]  # Generation color
-    colors_left_hand = ["#000000", ] * 5
-    colors_right_hand = ["#6a329f", ] * 5 
+    colors_orange = [
+        "#DD5A37",
+        "#D69E00",
+        "#B75A39",
+        "#FF6D00",
+        "#DDB50E",
+    ]  # Generation color
+    colors_left_hand = [
+        "#000000",
+    ] * 5
+    colors_right_hand = [
+        "#6a329f",
+    ] * 5
     colors_face = ["#00008B"]
     colors = colors_orange + colors_left_hand + colors_right_hand + colors_face
-    if vis_mode == 'upper_body':  # lower body taken fixed to input motion
+    if vis_mode == "upper_body":  # lower body taken fixed to input motion
         colors[0] = colors_blue[0]
         colors[1] = colors_blue[1]
-    elif vis_mode == 'gt':
+    elif vis_mode == "gt":
         colors = colors_blue
 
     frame_number = data.shape[0]
@@ -316,12 +444,17 @@ def plot_3d_motion(save_path, kinematic_tree, joints, title, dataset, figsize=(3
         #         print(index)
         ax.lines = []
         ax.collections = []
-        #ax.view_init(elev=0, azim=0)
+        # ax.view_init(elev=0, azim=0)
         ax.view_init(elev=120, azim=-90)
         ax.dist = 7.5
         #         ax =
-        plot_xzPlane(MINS[0] - trajec[index, 0], MAXS[0] - trajec[index, 0], 0, MINS[2] - trajec[index, 1],
-                     MAXS[2] - trajec[index, 1])
+        plot_xzPlane(
+            MINS[0] - trajec[index, 0],
+            MAXS[0] - trajec[index, 0],
+            0,
+            MINS[2] - trajec[index, 1],
+            MAXS[2] - trajec[index, 1],
+        )
         #         ax.scatter(dataset[index, :22, 0], dataset[index, :22, 1], dataset[index, :22, 2], color='black', s=3)
 
         # if index > 1:
@@ -338,16 +471,23 @@ def plot_3d_motion(save_path, kinematic_tree, joints, title, dataset, figsize=(3
                 linewidth = 4.0
             else:
                 linewidth = 2.0
-            ax.plot3D(data[index, chain, 0], data[index, chain, 1], data[index, chain, 2], linewidth=linewidth,
-                      color=color)
+            ax.plot3D(
+                data[index, chain, 0],
+                data[index, chain, 1],
+                data[index, chain, 2],
+                linewidth=linewidth,
+                color=color,
+            )
         #         print(trajec[:index, 0].shape)
 
-        plt.axis('off')
+        plt.axis("off")
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         ax.set_zticklabels([])
 
-    ani = FuncAnimation(fig, update, frames=frame_number, interval=1000 / fps, repeat=False)
+    ani = FuncAnimation(
+        fig, update, frames=frame_number, interval=1000 / fps, repeat=False
+    )
 
     # writer = FFMpegFileWriter(fps=fps)
     ani.save(save_path, fps=fps)
@@ -355,3 +495,42 @@ def plot_3d_motion(save_path, kinematic_tree, joints, title, dataset, figsize=(3
     # ani.save(save_path, writer='pillow', fps=1000 / fps)
 
     plt.close()
+
+
+def save_multiple_samples(
+    num_repetitions, out_path, row_print_template, row_file_template, rep_files
+):
+    all_rep_save_file = row_file_template
+    all_rep_save_path = os.path.join(out_path, all_rep_save_file)
+    ffmpeg_rep_files = [f" -i {f} " for f in rep_files]
+    hstack_args = (
+        f" -filter_complex hstack=inputs={num_repetitions}"
+        if num_repetitions > 1
+        else ""
+    )
+    ffmpeg_rep_cmd = (
+        "ffmpeg -y -loglevel warning "
+        + "".join(ffmpeg_rep_files)
+        + f"{hstack_args} {all_rep_save_path}"
+    )
+    os.system(ffmpeg_rep_cmd)
+    print(row_print_template.format(all_rep_save_file))
+
+
+def construct_template_variables(unconstrained):
+    row_file_template = "sample_all.mp4"
+    if unconstrained:
+        sample_file_template = "sample_rep{:02d}.mp4"
+        sample_print_template = "[rep #{:02d} | -> {}]"
+        row_print_template = "[all repetitions | -> {}]"
+    else:
+        sample_file_template = "sample_rep{:02d}.mp4"
+        sample_print_template = "[Rep #{:02d} | -> {}]"
+        row_print_template = "[all repetitions | -> {}]"
+
+    return (
+        sample_print_template,
+        row_print_template,
+        sample_file_template,
+        row_file_template,
+    )
